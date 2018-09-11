767793cc3382e4855fd3d41798673668b5b37f5f
Rafael Lopez <rafa@Rafaels-MacBook-Pro.local>
2018-09-11 00:47:36 -0600

    Shell lab(probably not right)

---
+++ shell/shell.py
@@ -0,0 +1,41 @@
+import os, sys, re
+
+pid = os.getpid()
+
+os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+
+print("Hint: type exit to quit.")
+args = ["", "shell.py"]
+
+while args[0] != "exit":
+    args[0] = input('shell-> ')
+    
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                 (os.getpid(), pid)).encode())
+        os.close(1)                 # redirect child's stdout
+        sys.stdout = open("shell-output.txt", "w")
+        fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+        os.set_inheritable(fd, True)
+        os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+        for dir in re.split(":", os.environ['PATH']): # try each directory in path
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly 
+
+        os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+        sys.exit(1) # terminate with error
+    else:          
+        # parent (forked ok)
+        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+                    (pid, rc)).encode())
+        childPidCode = os.wait()
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+                    childPidCode).encode())
